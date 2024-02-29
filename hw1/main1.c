fbe76cb58ba76e895489b3ac5999354e4987afae
Wei_Cheng Hung <138794467+WeiChengTW@users.noreply.github.com>
2024-02-29 14:44:06 +0800

    Create main1.c

---
+++ hw1/main1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[] = "AABBBCCCCddd";
+    int len_a = sizeof(a) / sizeof(a[0]);
+    // printf("%d\n\n", len_a);
+    char E = a[0];
+
+    for (int i = 1; i < len_a; i++)
+    {
+        int count = 1;
+        while (a[i] == E)
+        {
+            count += 1;
+            i++;
+        }
+        printf("%c%d", E, count);
+        E = a[i];
+    }
+}
